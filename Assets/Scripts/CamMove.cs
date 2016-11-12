805e5b4226f6f9fc67cb0aab867ed79a6f5ce4fa
EVE_TE\Timothi Ellim <timothiellim@gmail.com>
2016-11-12 12:23:39 -0800

    no message

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+
+	public Transform camposone;
+	public Transform campostwo;
+
+	public void FieldCam(){
+		this.gameObject.transform.position = campostwo.position;
+	}
+
+	public void KitchenCam(){
+		this.gameObject.transform.position = camposone.position;
+	}
+}
